6208e0c7d72a684184d4e2f859c55d2a8a0968bc
Nicolas Accetta <nicolas.accetta@gmail.com>
2024-05-12 13:26:57 -0300

    feat: use embed to include html and template files in binary

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"html/template"
 	"log"
 	"net/http"
@@ -12,11 +13,18 @@ type Film struct {
 	Director string
 }
 
+//go:embed index.html block/film.tmpl
+var files embed.FS
+
+func getTemplate() (*template.Template, error) {
+	return template.ParseFS(files, "index.html", "block/film.tmpl")
+}
+
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("index.html", "block/film.tmpl"))
+		t := template.Must(getTemplate())
 		films := map[string][]Film{
 			"Films": {
 				{Title: "The Godfather", Director: "Francis Ford Coppola"},
@@ -24,15 +32,15 @@ func main() {
 				{Title: "The Thing", Director: "John Carpenter"},
 			},
 		}
-		tmpl.Execute(w, films)
+		t.Execute(w, films)
 	})
 
 	mux.HandleFunc("POST /add-film/", func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(1 * time.Second)
 		title := r.PostFormValue("title")
 		director := r.PostFormValue("director")
-		tmpl := template.Must(template.ParseFiles("index.html", "block/film.tmpl"))
-		tmpl.ExecuteTemplate(w, "film", Film{Title: title, Director: director})
+		t := template.Must(getTemplate())
+		t.ExecuteTemplate(w, "film", Film{Title: title, Director: director})
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
